e99609d01a3e9b6a2731bd9dd4c0cbc9d795b99e
doroewel <dorowel@gmail.com>
2022-03-09 17:44:25 -0300

    Loop com while

--- teste.php
+++ teste.php
@@ -1,4 +1,15 @@
 
 <?php
 
-echo " Teste de ambiente";
+echo " Teste de ambiente".PHP_EOL; 
+//pular linha
+
+// Contador com o While //
+$contador = 0;
+
+while ($contador <=10){
+
+    echo $contador.PHP_EOL;
+
+    $contador = $contador +1;
+}
